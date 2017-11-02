9e99d2935e2a3e541cf4669f9315cd270364e4b4
beyzacardak <beyzaaa.cardak@gmail.com>
2017-11-02 12:56:50 +0200

    n

---
+++ stringcalsma/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringcalsma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string metin = Console.ReadLine();
+            //metin = metin.Replace(".", "").Replace(",", "").Replace("'", "").Replace("\"", "").Replace("?", "").Replace("!", "");
+            var kelimeler = metin.Split(new[] {' ','.',',','?','!' });
+
+
+            string uzunkelime = "";
+            foreach(var kelime in kelimeler)
+            {
+                if(kelime.Length >uzunkelime.Length)
+                {
+                    uzunkelime = kelime;
+                }
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+        }
+    }
+}
