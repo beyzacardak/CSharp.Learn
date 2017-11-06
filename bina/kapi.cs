851342753b8428a2d17e6ce1269f7d41b2d39a34
beyzacardak <beyzaaa.cardak@gmail.com>
2017-11-06 10:03:27 +0200

    m

---
+++ bina/kapi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bina
+{
+    class kapi
+    {
+        public string govde;
+        public string kulp;
+        public string kilit;
+
+        public void kapiyiAc()
+        {
+            Console.WriteLine("kapı açıldı");
+        }
+    }
+}
